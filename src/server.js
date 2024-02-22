624395944d0ead71b6cc7a3654b588b0705bf7fd
martinnss <martin.olivarest@utem.cl>
2024-02-21 22:08:49 -0300

    ip

--- src/server.js
+++ src/server.js
@@ -15,6 +15,7 @@ const app = express()
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Agrega tu dominio local
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   //cors({ origin: 'http://localhost:3000' });
   next();
 });
